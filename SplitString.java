a408325ad940d2c6a2ce862667d95cc8b9a63ce2
Harni17 <97238928+Harni17@users.noreply.github.com>
2022-01-10 19:43:01 +0530

    Add files via upload

---
+++ SplitString.java
@@ -0,0 +1,26 @@
+package com.harni.assignment;
+
+import java.util.Scanner;
+
+public class SplitString {
+		public static void main(String[] args) {
+			System.out.println(" enter the string : ");
+			Scanner scan = new Scanner(System.in);
+			String str = scan.nextLine();
+			String Delimiter = " ";
+			splitfunc(str,Delimiter);
+				
+			}
+		static void splitfunc(String str, String delimiter) {
+			for(int i=0;i<str.length();i++) {
+				if(str.charAt(i)==' ') {
+					System.out.println();
+					continue;
+				}
+				System.out.println(str.charAt(i));
+			
+		}
+				
+			}
+		}
+	
